ca31e815fad8a6f480e46fc29177bebabca5f0fc
Y_ishizuka <atwonder39@gmail.com>
2014-11-13 00:36:43 +0900

    人数選択からゲームスタートまでの導線の追加

--- OgiriBattle/Assets/Script/GameManager.cs
+++ OgiriBattle/Assets/Script/GameManager.cs
@@ -15,6 +15,10 @@ public class GameManager : MonoBehaviour {
 	public int SLIDESPEED;
 	public float SLIDEDIS;
 	float timer;
+
+	bool GAMESTART = false;
+	bool CONFIRM = false;
+
 	bool SLIDING = false;
 	// Use this for initialization
 	void Start () {
@@ -24,7 +28,9 @@ public class GameManager : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		SlideOut ();
-		if(playerNum != 0){
+		if(playerNum == 999){
+
+		}else if(playerNum != 0){
 			PlayerSelectConfirmMes.text = playerNum + "人で開始します\nよろしいですか？";
 		}
 	}
@@ -50,6 +56,16 @@ public class GameManager : MonoBehaviour {
 		SLIDING = true;
 	}
 
+	void ReturnSelect(){
+		Debug.Log("Return");
+
+	}
+
+	void ConfirmSelect(){
+		SLIDING = true;
+		CONFIRM = true;
+	}
+
 	void SlideOut(){
 		if(playerNum != 0){
 			if(SLIDING){
@@ -58,7 +74,13 @@ public class GameManager : MonoBehaviour {
 				Debug.Log(Time.deltaTime);
 				if(timer<= 0.0f){
 					SLIDING = false;
+					timer = SLIDEDIS;
+					if(CONFIRM){
+						GAMESTART = true;
+						Debug.Log("gameStart");
+					}
 				}
+
 			}else{
 			}
 		}
