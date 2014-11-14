b73738f6c7fc07b59f4c604dffd5b79771673be0
Y_ishizuka <atwonder39@gmail.com>
2014-11-14 18:41:59 +0900

    InGameのController：GameControllerの追加

---
+++ OgiriBattle/Assets/Script/GameController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	float countDownTimer = 3;
+	public UILabel timerLb;
+	bool COUNTDOWNING = false;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (COUNTDOWNING) {
+			countDownTimer -= Time.deltaTime;
+			int timerInt = (int)Mathf.Round (countDownTimer);
+			timerLb.text = timerInt.ToString ();
+		}
+	}
+}
